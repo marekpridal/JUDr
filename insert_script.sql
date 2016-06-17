c67e0d7b0c1d0ad4a1ac9e099af11642ac9f17d3
Marek Přidal <m.pridal29@gmail.com>
2016-06-17 21:16:06 +0200

    Oprava unique v databázi

--- insert_script.sql
+++ insert_script.sql
@@ -5,7 +5,7 @@ insert into zamestnanci(Jmeno,Prijmeni,username,password,vlozil) values("Marek",
 insert into zamestnanci(username,password) values("admin","admin");
 
 select * from zamestnanci;
-delete from zamestnanci;
+/*delete from zamestnanci;*/
 
 insert into klienti(Jmeno,Prijmeni,Telefon,Email,Ulice,Mesto,PSC,Stat) VALUES("Pavel","Mlčoch",456987231,"pavel.mlcoch@email.cz","Pod Stromem 5","Horní Dolní","50020","Česká republika");
 insert into klienti(ICO,Nazev,Telefon,Email,Ulice,Mesto,PSC,Stat) VALUES("25596641","ZAPA",456987258,"info@zapa.cz","Pod Mostem 41","Karlovy Vary","40020","Česká republika");
